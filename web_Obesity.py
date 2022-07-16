45765897dfe516e03795372175aae3ce2ccd801f
Luis Garcia <garcialuis783@gmail.com>
2022-07-16 02:29:20 -0500

    Fix Finish :(

--- web_Obesity.py
+++ web_Obesity.py
@@ -33,7 +33,7 @@ def hacer_request_api(Genero_, Edad_, Historial_familiar_, C_rico_calorias_, F_C
     url_api = "https://team5-diplo.herokuapp.com/predict"
 
     pred = requests.post(url=url_api, json=json.loads(data_cleaned),headers={"Content-Type": "application/json"}).text
-    pred_df = pd.read_json(pred)
+    pred_df = json.loads(pred)
     return pred_df[0]
 
 def grafP(c2):
