f0bce8a8ab8a23dbe780f45899b5fa740b87cd96
Luis Garcia <garcialuis783@gmail.com>
2022-07-15 23:18:26 -0500

    Fix

--- Clases.py
+++ Clases.py
@@ -7,18 +7,18 @@ import pandas as pd
 
 class InputModelo(BM):
         
-    Genero: Literal["Female", "Male"]
+    Genero: Literal["Femenino", "Masculino"]
     Edad: int = Field( ge=0, description="Edad de la persona")
-    Historial_familiar: Literal["yes", "no"]
-    C_rico_calorias: Literal["yes", "no"]
+    Historial_familiar: Literal["Si", "No"]
+    C_rico_calorias: Literal["Si", "No"]
     F_Consumo_verduras: int = Field( ge=1, le=3, description="Frecuencia con la que come verduras")
     N_comidas: int = Field( ge=1, le=4, description="Numero de comidas consnumidas al dia")
-    Meriendas: Literal['Sometimes', 'Frequently', 'Always', 'no']
-    Fumador: Literal["yes", "no"]
+    Meriendas: Literal['Algunas_veces', 'Frecuentemente', 'Siempre', 'No']
+    Fumador: Literal["Si", "No"]
     F_actvidad_fisica: float = Field( ge=0, le=3, description="Frecuencia con la que realiza actividad fisica")
     T_uso_dispositivos: float = Field(ge=0, le=3, description="Tiempo de uso de dispositivo elctronicos")
-    Consumo_calorias: Literal["yes", "no"]
-    C_alcohol: Literal['no', 'Sometimes', 'Frequently', 'Always']
+    Consumo_calorias: Literal["Si", "No"]
+    C_alcohol: Literal["No", "Algunas_veces", "Frecuentemente", "Siempre"]
     Medio_transporte: Literal['Public_Transportation', 'Walking', 'Automobile', 'Motorbike','Bike']
     
     
